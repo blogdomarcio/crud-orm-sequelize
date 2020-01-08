14d4526ba4d42c770dc9f52803ae7057b1063c58
Marcio <blogdomarcio@live.com>
2020-01-08 15:33:10 -0300

    Fix

--- routes/pessoas.js
+++ routes/pessoas.js
@@ -14,5 +14,4 @@ router.get('/excluir/:id', pessoasController.deleteOne.bind(null, model.models))
 router.get('/update/:id', pessoasController.updateForm.bind(null, model.models))
 router.post('/update/:id', pessoasController.updateProcess.bind(null, model.models))
 
-
 module.exports = router
